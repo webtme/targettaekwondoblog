e91fb1257f5691d4f170790b3a835887045045b9
webtme <webtme@hotmail.com>
2024-12-20 23:09:42 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,8 +27,6 @@ export default function Home() {
             <ProjectSection />
             <BlogSection />
             <FeaturesSection />
-            <LogoFade />
-            <LogoSection />
           </div>
           <div
             className="-z-50 absolute inset-0 bg-grid-slate-50 [mask-image:linear-gradient(0deg,#fff,rgba(255,255,255,0.3))] dark:bg-grid-slate-700/25 dark:[mask-image:linear-gradient(0deg,rgba(255,255,255,0.1),rgba(255,255,255,0.5))]"
