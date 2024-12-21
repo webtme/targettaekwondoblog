c027b23edbdddbcee4e4f74328711135374b14d8
webtme <webtme@hotmail.com>
2024-12-22 00:31:16 +0800

    Update index.jsx

--- src/components/landing/logoSection/index.jsx
+++ src/components/landing/logoSection/index.jsx
@@ -13,37 +13,6 @@ const LogoFade = () => {
           <div className="overlay">Google</div>
         </a>
        </div>
-       <div className="logo-item">
-        <a href="https://www.microsoft.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://hololivepro.com/wp-content/uploads/2023/03/officialshop_hp.png" alt="Microsoft" className="logo fade-in" />
-          <div className="overlay">Microsoft</div>
-        </a>
-       </div>
-       <div className="logo-item">
-        <a href="https://www.amazon.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://hololivepro.com/wp-content/uploads/2023/03/hololive_hp.png" alt="Amazon" className="logo fade-in" />
-          <div className="overlay">Amazon</div>
-        </a>
-       </div>
-       
-       <div className="logo-item">
-        <a href="https://www.google.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://hololivepro.com/wp-content/uploads/2023/03/holostars_hp.png" alt="Google" className="logo fade-in" />
-          <div className="overlay">Google</div>
-        </a>
-       </div>
-       <div className="logo-item">
-        <a href="https://www.microsoft.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://hololivepro.com/wp-content/uploads/2023/03/officialshop_hp.png" alt="Microsoft" className="logo fade-in" />
-          <div className="overlay">Microsoft</div>
-        </a>
-       </div>
-       <div className="logo-item">
-        <a href="https://www.amazon.com" target="_blank" rel="noopener noreferrer">
-          <img src="https://hololivepro.com/wp-content/uploads/2023/03/hololive_hp.png" alt="Amazon" className="logo fade-in" />
-          <div className="overlay">Amazon</div>
-        </a>
-      </div>
     </div>
    </div>
   );
