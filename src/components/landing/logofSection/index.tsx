82ef0389c5dd2e0ed8576b64b254ddca56b1a374
webtme <webtme@hotmail.com>
2024-12-22 00:03:15 +0800

    Update index.tsx

--- src/components/landing/logofSection/index.tsx
+++ src/components/landing/logofSection/index.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent } from "../src/components/magicui/card";
+import { Card, CardContent } from "../components/magicui/card";
 
 const stats = [
   { value: "10K+", label: "Active Users" },
